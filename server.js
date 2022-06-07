41b6406ea9225a213e7cadeb215512ca2f2b2a5a
spockula <chinedukogu@gmail.com>
2022-06-07 08:18:05 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/lend-sqr-test-test'));
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/lend-sqr-test'));
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/lend-sqr-test/index.html'));});
+    
+res.sendFile(path.join(__dirname+'/dist/lend-sqr-test/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
